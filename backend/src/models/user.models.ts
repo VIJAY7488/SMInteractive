924ecc49934b4be4ac996eb823d842d61f09054d
Vijay Patel <vijaypatel3207@gmail.com>
2025-11-08 14:02:15 +0530

    add name

--- backend/src/models/user.models.ts
+++ backend/src/models/user.models.ts
@@ -2,6 +2,7 @@ import { Schema, model } from 'mongoose';
 
 
 export interface IUser {
+name: string;
 email: string;
 password: string;
 role: 'admin' | 'user';
@@ -11,6 +12,7 @@ createdAt?: Date;
 
 
 const userSchema = new Schema<IUser>({
+name: { type: String, required: true },
 email: { type: String, required: true, unique: true },
 password: { type: String, required: true },
 role: { type: String, enum: ['admin', 'user'], default: 'user' },
