6536cc886a3207c0fb549e0141f169f69fc6d76a
Vijay Patel <vijaypatel3207@gmail.com>
2025-11-11 11:06:44 +0530

    add api routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,25 @@
 import express from 'express';
+import cookiesParser from 'cookie-parser'; 
+import authRoutes from './routes/auth.routes';
+import spinWheelRoutes from './routes/spinWheel.routes';
+import transactionRoutes from './routes/transaction.routes';
+import userRoutes from './routes/user.routes';
+import adminRoutes from './routes/admin.routes';
 
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cookiesParser());
 
 
-
+// API Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/spin-wheels', spinWheelRoutes);
+app.use('/api/transactions', transactionRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/admin', adminRoutes);
 app.get('/', (req, res) => res.send('Roxstar Spin Wheel Backend'));
 
 
